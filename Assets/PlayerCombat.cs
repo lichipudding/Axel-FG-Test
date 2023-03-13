48cc65f3e6cc845af904909d97144cafc9e2535f
axelpaxelprog <72688107+axelpaxelprog@users.noreply.github.com>
2023-03-13 21:52:12 +0100

    Lightfade coroutine in place

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -9,26 +9,18 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public Light attackFlash;
-    private float lightDuration = 20f;
+    private float waitBetweenAttacks = 1f;
 
     public LayerMask enemyLayer;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        // 
+        if (Input.GetKeyDown(KeyCode.E) && waitBetweenAttacks >= 1f)
         {
             Attack();
         }
-
-        //not super broken, adjust durations & fix hangup
-        if (attackFlash.intensity >= 0.1f && lightDuration > 0f)
-        {
-            attackFlash.intensity -= 0.1f;
-
-            lightDuration -= 0.1f;
-        }
-
     }
 
     void Attack()
@@ -39,10 +31,31 @@ public class PlayerCombat : MonoBehaviour
 
         foreach (Collider enemy in hitEnemies)
         {
-            attackFlash.intensity = 5f;
             Debug.Log("HIT" + enemy.name);
-            //attackFlash.intensity = 0f;
+            StartCoroutine("LightFade");
         }
+    }
+
+
+
+    private IEnumerator LightFade()
+    {
+        float lightDuration = 1f;
+        float interval = 0.01f;
+        attackFlash.intensity = 5f;
+
+        while (lightDuration >= 0.0f)
+        {
+            attackFlash.intensity -= 0.1f;
+
+            lightDuration -= interval;
+
+            waitBetweenAttacks -= 0.1f;
+
+            yield return new WaitForSeconds(interval);
+        }
+        
+        waitBetweenAttacks = 1f;
     }
 
     private void OnDrawGizmosSelected()
